f8848ffe07b7b0cb05f5cf81ee7da3334fc898f0
prerna vashistha <prernavas27@gmail.com>
2021-06-08 19:28:17 +0530

    first

---
+++ intro1.js
@@ -0,0 +1,24 @@
+let a= 100 ;
+if(true){
+    let a=500;
+    console.log(a);
+}
+console.log(a);
+let avenger ={
+    name: "steve",
+    place: "new york",
+    skills: [
+        "dance",
+        'arts',
+        'takwondo',
+        'martila arts',
+        {
+            bestfriend : [
+                  {sname: 'natsha', skills: []},
+                  {sname: 'tony', skills: ['brain']}
+            ]
+        } 
+    ]
+}
+console.log(avenger.skills[4].bestfriend);
+
---
+++ test.js
@@ -0,0 +1,6 @@
+let a= 100 ;
+if(true){
+    let a=200;
+    console.log(a);
+}
+console.log(a);
