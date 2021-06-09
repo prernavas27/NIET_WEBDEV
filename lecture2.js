d063dab55900fc51c825e192c4c10a9e73a64245
prernavas27@gmai.com <prernavas27@gmail.com>
2021-06-09 10:25:49 +0530

    hi

--- lecture2.js
+++ lecture2.js
@@ -1,1 +1,22 @@
-//function 
+//function body
+function callme(name){
+    console.log("hi am prerna ");
+    console.log(" hi "+name);
+return 10;
+}
+//calling the function
+//console.log(callme());
+//console.log(callme);
+//undefined because it return no value
+let val=callme();
+//console.log(val);
+
+//
+//callme('steve');
+sayhi();
+//call back functions ans high order functions
+let sayhi; //undefined 
+sayhi = function(){
+console.log('i am saying hii');
+}  //sayhi can take function 
+//sayhi();
